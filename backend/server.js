68d0531bd8f0d6d13fb6e414e94d1b651cb8e9e1
Tuliany <tuliany@mail.com>
2020-05-24 11:17:50 +0200

    registration and login created

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,37 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import crypto from 'crypto'
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt-nodejs'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+mongoose.set('useCreateIndex', true);
+
+
+const User = mongoose.model('User', {
+  name: {
+    type: String, 
+    unique: true
+  },
+  email:{
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken:{
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+
+// const user = new User({name: "Bob", password: bcrypt.hashSync("testing")})
+// user.save()
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -13,6 +39,17 @@ mongoose.Promise = Promise
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
+
+// Middleware
+const authenticateUser = async (req, res, next) => {
+  const user = await User.findOne({accessToken: req.header('Authorization')})
+  if (user){
+    req.user = user;
+    next()
+  } else {
+    res.status(401).json({loggedOut: true})
+  }
+}
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
@@ -23,7 +60,41 @@ app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
+//protected endpoint
+app.get('/secrets', authenticateUser)
+app.get('/secrets', (req,res) =>{
+  res.json({secret: 'This is a super secret message'})
+})
+
+//Registration (creates the user)
+app.post('/users', async (req,res)=>{
+  try{
+    const {name, email, password} = req.body
+    const user = new User({name, email, password: bcrypt.hashSync(password)})
+    user.save()
+    res.status(201).json({id: user._id, accessToken: user.accessToken})
+  } catch (err){
+    res.status(400).json({message: 'could not create user', errors: err.erros})
+
+  }
+})
+
+//Login (finds the user)
+app.post('/sessions', async (req, res) => {
+  const user = await User.findOne({email: req.body.email})
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+  //Sucess
+  res.json({userId: user._id, accessToken: user.acessToken})
+  } else {
+  //Fail
+  res.json({notFound: true})
+  }
+})
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
+
+//console.log(crypto.randomBytes(1).toString('hex'))
+//console.log(bcrypt.hashSync('testing'))
